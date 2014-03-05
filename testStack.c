61258e3cb204e30a7461e21975dcfa17f0b5f12c
Lichard <ryli721@gmail.com>
2014-03-05 14:57:09 -0500

    Initial Commit. Code for Stack and testStack.

---
+++ testStack.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<obliv.h>
+#include"testStack.h"
+
+int main(int argc, char*argv[])
+{
+    ProtocolDesc pd;
+    protocolIO io;
+    if(argc<3)
+    {
+        if(argc<2) fprintf(stderr, "Party missing\n");
+        else fprintf(stderr, "data missing");
+        return 1;
+    }
+    else if(argc>12) 
+    {
+        fprintf(stderr, "Too many entries");
+        return 1;
+    }
+
+    io->size = argc-2;
+    for(int i=2; i<argc; i++)
+        sscanf(argv[i], "%d",io->data[i]); 
+    protocolUseStdio(&pd);
+    setCurrentParty(&pd, argv[1][0]=='1'?1:2);
+    execYaoProtocol(&pd, hamming, io);
+    cleanupProtocol(&pd);
+    fprintf(stderr, "Result: %d\n", io.hammingDist);
+}
---
+++ testStack.h
@@ -0,0 +1,8 @@
+typedef struct protocolIO
+{
+    int size;
+    int hammingDist;
+    int data[10];
+}
+
+void hamming(void* args);

9ccbcaa94da78c66af41d8828b71c2e8a65262a5
Lichard <ryli721@gmail.com>
2014-04-04 15:10:35 -0400

    Implementation completed. Added oBoolCopy. Compiles with no errors or warnings. Still needs full testing

--- testStack.c
+++ testStack.c
@@ -6,6 +6,7 @@ int main(int argc, char*argv[])
 {
     ProtocolDesc pd;
     protocolIO io;
+	/*
     if(argc<3)
     {
         if(argc<2) fprintf(stderr, "Party missing\n");
@@ -17,13 +18,16 @@ int main(int argc, char*argv[])
         fprintf(stderr, "Too many entries");
         return 1;
     }
-
-    io->size = argc-2;
-    for(int i=2; i<argc; i++)
-        sscanf(argv[i], "%d",io->data[i]); 
+	*/
+    io.size = argc-2;
+	int i;
+    for(i=2; i<argc; i++)
+        sscanf(argv[i], "%d",&io.data[i]); 
+	
     protocolUseStdio(&pd);
     setCurrentParty(&pd, argv[1][0]=='1'?1:2);
-    execYaoProtocol(&pd, hamming, io);
+    execYaoProtocol(&pd, testStack, &io);
     cleanupProtocol(&pd);
-    fprintf(stderr, "Result: %d\n", io.hammingDist);
+    //fprintf(stderr, "Result: %d\n", io.hammingDist);
+	fprintf(stderr, "Finished running\n");
 }
--- testStack.h
+++ testStack.h
@@ -3,6 +3,6 @@ typedef struct protocolIO
     int size;
     int hammingDist;
     int data[10];
-}
+} protocolIO;
 
-void hamming(void* args);
+void testStack(void* args);
